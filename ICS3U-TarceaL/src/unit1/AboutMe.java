c4c457de0c6462a5126a626d9ca073185435a9ba
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2024-09-20 11:39:13 -0400

    Adding ICS3U project with AboutMe class

---
+++ ICS3U-TarceaL/src/unit1/AboutMe.java
@@ -0,0 +1,16 @@
+package unit1;
+/**
+ * Description: This program prints my information
+ * Date: 20/09/24
+ * @author Luca Tarcea
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		System.out.println("Luca Tarcea");
+		System.out.println("Grade 11");
+		System.out.println("Ms. Kemp");
+		System.out.println("Lawrence Park CI");
+	}
+
+}
