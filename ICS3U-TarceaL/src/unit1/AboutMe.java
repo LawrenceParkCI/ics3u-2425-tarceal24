1f5a3b6e0eff9c4fe611f58529eea7ef8747b7df
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2024-09-23 09:45:15 -0400

    Added formatting to my AboutMe class.

--- ICS3U-TarceaL/src/unit1/AboutMe.java
+++ ICS3U-TarceaL/src/unit1/AboutMe.java
@@ -7,10 +7,17 @@ package unit1;
 public class AboutMe {
 
 	public static void main(String[] args) {
+		// This prints the information to the console
 		System.out.println("Luca Tarcea");
 		System.out.println("Grade 11");
 		System.out.println("Ms. Kemp");
 		System.out.println("Lawrence Park CI");
+		System.out.println("\"Go Panthers!\"");
+		System.out.format("\nDay: %-20sStart Time: %s", "Monday", "9:00");
+		System.out.format("\nDay: %-20sStart Time: %s", "Tuesday", "9:00");
+		System.out.format("\nDay: %-20sStart Time: %s", "Wednesday", "9:55");
+		System.out.format("\nDay: %-20sStart Time: %s", "Thursday", "9:00");
+		System.out.format("\nDay: %-20sStart Time: %s", "Friday", "9:00");
 	}
 
 }
