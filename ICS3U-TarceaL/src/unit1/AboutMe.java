9a431dd3179f728ddbc116b348750b7b53fe307c
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2024-09-24 11:40:31 -0400

    Updating AboutMe class

--- ICS3U-TarceaL/src/unit1/AboutMe.java
+++ ICS3U-TarceaL/src/unit1/AboutMe.java
@@ -13,11 +13,29 @@ public class AboutMe {
 		System.out.println("Ms. Kemp");
 		System.out.println("Lawrence Park CI");
 		System.out.println("\"Go Panthers!\"");
-		System.out.format("\nDay: %-20sStart Time: %s", "Monday", "9:00");
-		System.out.format("\nDay: %-20sStart Time: %s", "Tuesday", "9:00");
-		System.out.format("\nDay: %-20sStart Time: %s", "Wednesday", "9:55");
-		System.out.format("\nDay: %-20sStart Time: %s", "Thursday", "9:00");
-		System.out.format("\nDay: %-20sStart Time: %s", "Friday", "9:00");
+		//System.out.printf("Day:     |  Period 1  |  Period 2   |  Lunch  |   Period 3   |  Period 4");
+		//System.out.printf("\nMonday:   | 9:00-10:15 | 10:20-11:40 |  Lunch  |  12:40-1:55  |  Period 4");
+		//System.out.printf("\nTuesday:   | 9:00-10:15 | 10:20-11:40 |  Lunch  |  12:40-1:55  |  Period 4");
+		//System.out.printf("\nWednesday: | 9:55-10:55 | 11:00-12:00 |  Lunch  |   1:00-2:05  |  Period 4");
+		//System.out.printf("\nThursday:  | 9:00-10:15 | 10:20-11:40 |  Lunch  |  12:40-1:55  |  Period 4");
+		//System.out.printf("\nFriday:   | 9:00-10:15 | 10:20-11:40 |  Lunch  |  12:40-1:55  |  Period 4");
+		System.out.format("\n  Day%15s%15s%17S%15s%15s", "Period 1","Period 2","Lunch","Period 3","Period 4");
+		System.out.format("\n  Monday:%16s%15s%10S%15s%15s","9:00-10:15  |","10:20-11:40 |","Lunch  |","12:40-1:55  |","2:00-3:15  |");
+		System.out.format("\nClass:%20s%15s%10S%15s%15s","Computer Science","Accounting","Lunch","Chemistry","English");
+		System.out.println("\n________________________________________________________________________________");
+		System.out.format("\n  Tuesday:%15s%15s%10S%15s%15s","9:00-10:15 |","10:20-11:40 |","Lunch  |","12:40-1:55  |","2:00-3:15  |");
+		System.out.format("\nClass:%15s%20s%10S%15s%15s","Accounting","Computer Science","Lunch","English","Chemistry");
+		System.out.println("\n________________________________________________________________________________");
+		System.out.format("\n Wednesday:%13s%15s%10S%15s%15s","9:00-10:15","10:20-11:40","Lunch","12:40-1:55","2:00-3:15");
+		System.out.format("\nClass:%20s%15s%10S%15s%15s","Computer Science","Accounting","Lunch","Chemistry","English");
+		System.out.println("\n________________________________________________________________________________");
+		System.out.format("\n Thursday:%14s%15s%10S%15s%15s","9:00-10:15","10:20-11:40","Lunch","12:40-1:55","2:00-3:15");
+		System.out.format("\nClass:%15s%20s%10S%15s%15s","Accounting","Computer Science","Lunch","English","Chemistry");
+		System.out.println("\n________________________________________________________________________________");
+		System.out.format("\n  Friday:%16s%15s%10S%15s%15s","9:00-10:15","10:20-11:40","Lunch","12:40-1:55","2:00-3:15");
+		System.out.format("\nClass:%20s%15s%10S%15s%15s","Computer Science","Accounting","Lunch","Chemistry","English");
+		System.out.println("\n________________________________________________________________________________");
+		
 	}
 
 }
