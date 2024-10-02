06a2a3016b684dc87b96c3484aa35b1cb9cb732a
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2024-10-02 11:26:07 -0400

    Added InputFP class to unit1 package

---
+++ ICS3U-TarceaL/src/unit1/InputFP.java
@@ -0,0 +1,24 @@
+package unit1;
+import java.util.Scanner;
+/**
+ * 
+ */
+public class InputFP {
+/**
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double num1,num2;
+		System.out.println("Type in a floating point number and <Enter>");
+		num1 = sc.nextDouble();
+		System.out.println("Type in another floating point number and <Enter>");
+		num2 = sc.nextDouble();
+		System.out.println();
+		System.out.println("The first number entered was "+num1);
+		System.out.println("The second number entered was "+num2);
+
+	}
+
+}
