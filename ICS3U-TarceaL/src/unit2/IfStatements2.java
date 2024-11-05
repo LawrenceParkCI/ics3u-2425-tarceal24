554f7a512e1632dc510e8f95b4e0d23917daa6fa
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2024-11-05 09:39:07 -0500

    Added method comment.

--- ICS3U-TarceaL/src/unit2/IfStatements2.java
+++ ICS3U-TarceaL/src/unit2/IfStatements2.java
@@ -9,6 +9,10 @@ import java.util.Scanner;
  */
 
 public class IfStatements2 {
+	/**
+	 * Entry point to the program.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// Scanner in = new Scanner(System.in);
 
