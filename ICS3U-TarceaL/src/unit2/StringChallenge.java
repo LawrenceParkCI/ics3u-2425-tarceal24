77d288134282c066f17b52a2fc31d8698139c0a7
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2024-11-11 10:01:38 -0500

    Added StringChallenge class to unit2 package

--- ICS3U-TarceaL/src/unit2/StringChallenge.java
+++ ICS3U-TarceaL/src/unit2/StringChallenge.java
@@ -3,23 +3,36 @@ package unit2;
 import java.util.Scanner;
 
 /**
- * Description:
- * Date:
- * @author 
-*/
+ * Description: 
+ * Date: 11/11/24
+ * @author Luca Tarcea
+ */
 
 public class StringChallenge {
-  public static void main(String[] args) {
-    //This is for the portfolio
+	/**
+	 * Entry point to the program.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// This is for the portfolio
+		/*
+		 * Create a program that demonstrates the use of .equals(), .equalsIgnoreCase(),
+		 * .compareTo(). You should be using if statements for this program.
+		 * 
+		 * Also, include an example of explicit and implicit casting.
+		 * 
+		 * Comment to explain: -when you use the different String functions -when you
+		 * are casting and how the value is changed
+		 */
+		System.out.println("Type in a sentence and <Enter>");
+		String str = sc.next();
+		System.out.println(str);
+		System.out.println(str.equals(""));
+		System.out.println(str.equalsIgnoreCase(""));
+		System.out.println(str.compareTo(""));
 
-    /*
-    Create a program that demonstrates the use of .equals(), .equalsIgnoreCase(), .compareTo().  You should be using if statements for this program.
-
-    Also, include an example of explicit and implicit casting.
-
-    Comment to explain:
-      -when you use the different String functions
-      -when you are casting and how the value is changed
-    */
-  }
+		sc.close();
+	}
 }
