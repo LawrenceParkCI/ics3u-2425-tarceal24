feec1226483b862013feff74eba437f04064e974
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2024-12-19 10:16:02 -0500

    Added methods to MathPlus class

--- ICS3U-TarceaL/src/unit3/MathPlus.java
+++ ICS3U-TarceaL/src/unit3/MathPlus.java
@@ -14,11 +14,36 @@ public class MathPlus {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("The distance from (2, -1) to (-3, -2) is " + distance(2, -3, -1, -2) + ".");
-		System.out.println("The hypotenuse of a triangle with side lengths 3 and 4 is " + hypotenuse(3, 4) + ".");
-		System.out.println("The number factors in 20 is " + numOfFactors(20) + ".");
-		System.out.println("13 is prime: " + isPrime(13) + ".");
+//		System.out.println("The distance from (2, -1) to (-3, -2) is " + distance(2, -3, -1, -2) + ".");
+//		System.out.println("The hypotenuse of a triangle with side lengths 3 and 4 is " + hypotenuse(3, 4) + ".");
+//		System.out.println("The number factors in 20 is " + numOfFactors(20) + ".");
+//		System.out.println("13 is prime: " + isPrime(13) + ".");
+//		int[] array1 = {1, 2, 3, 4, 5};
+//		System.out.println("The sum of all the integers in the array is " + sum(array1) + ".");
+//		double[] array2 = {3.6, 4.2, 2.3, 2};
+//		System.out.println("The sum of all the doubles in the array is " + (double) ((int) (sum(array2)*10))/10 + ".");
+//		int[] array3 = {1, 2, 3, 0, 5};
+//		System.out.println("The index of the smallest integer in the array is " + min(array3) + ".");
+//		double[] array4 = {30, 22, 33, 26, 17};
+//		System.out.println("The index of the smallest double in the array is " + min(array4) + ".");
+//		int[] array5 = {10, 12, 23, 14, 18};
+//		System.out.println("The index of the largest integer in the array is " + max(array5) + ".");
+//		double[] array6 = {23.1, 23.2, 16, 10, 8};
+//		System.out.println("The index of the largest double in the array is " + max(array6) + ".");
+//		int[] array7 = {81, 23, 68, 29, 10};
+//		System.out.println("The difference of the largest and smallest integers in the array is " + bigDifference(array7) + ".");
+//		double[] array8 = {81.3, 23.2, 68.5, 29.8, 10};
+//		System.out.println("The difference of the largest and smallest doubles in the array is " + bigDifference(array8) + ".");
+		int num = 100;
+		System.out.println("The factors of " + num + " are:");
+		for (int i = 0; i < num; i++) {
+			if (factors(num)[i] != 0)
+				System.out.println(factors(num)[i]);
+		}
+		System.out.println(num);
+		
 	}
+	
 
 	/**
 	 * Description: Calculates the distance between two given points
@@ -73,5 +98,95 @@ public class MathPlus {
 			return true;
 		else
 			return false;
+	}
+
+	public static int sum(int[] num) {
+		int total = 0;
+		for (int i = 0; i < num.length; i++) {
+			total += num[i];
+		}
+		return total;
+	}
+
+	public static double sum(double[] num) {
+		double total = 0;
+		for (int i = 0; i < num.length; i++) {
+			total += num[i];
+		}
+		return total;
+	}
+	
+	public static int min(int[] num) {
+		int index = 0;
+		int least = num[0];
+		for (int i = 0; i < num.length; i++) {
+			if (least > num[i]) {
+				least = num[i];
+				index = i;
+			}
+		}
+		return index;
+	}
+	
+	public static int min(double[] num) {
+		int index = 0;
+		double least = num[0];
+		for (int i = 0; i < num.length; i++) {
+			if (least > num[i]) {
+				least = num[i];
+				index = i;
+			}
+		}
+		return index;
+	}
+	
+	public static int max(int[] num) {
+		int index = 0;
+		int greatest = num[0];
+		for (int i = 0; i < num.length; i++) {
+			if (greatest < num[i]) {
+				greatest = num[i];
+				index = i;
+			}
+		}
+		return index;
+	}
+	
+	public static int max(double[] num) {
+		int index = 0;
+		double greatest = num[0];
+		for (int i = 0; i < num.length; i++) {
+			if (greatest < num[i]) {
+				greatest = num[i];
+				index = i;
+			}
+		}
+		return index;
+	}
+	
+	public static int bigDifference(int[] num) {
+		int least = min(num);
+		int greatest = max(num);
+		int difference = num[greatest] - num[least];
+		return difference;
+	}
+	
+	public static double bigDifference(double[] num) {
+		int least = min(num);
+		int greatest = max(num);
+		double difference = num[greatest] - num[least];
+		return difference;
+	}
+	
+	public static int[] factors(int num) {
+		int[] array = new int[100];
+		int index = 0;
+		for (int i = 1; i < num; i++) {
+			if (num % i == 0) {
+				array[index] = i;
+				index++;
+			}
+		}
+		return array;
 	}
 }
