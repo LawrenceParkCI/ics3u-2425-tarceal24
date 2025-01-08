43973db1f387873342044a1fd0e65215b8bfe2d5
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2025-01-08 12:00:35 -0500

    Added rowCheck method

--- ICS3U-TarceaL/src/unit3/CulminatingAssignment.java
+++ ICS3U-TarceaL/src/unit3/CulminatingAssignment.java
@@ -1,25 +1,31 @@
 package unit3;
 
 import hsa_new.Console;
-import java.util.Scanner;
 import java.awt.Color;
 
 public class CulminatingAssignment {
 	/**
 	 * Entry point to the program.
 	 * @param args
-	 */
-	static Scanner sc;
+	 */	
 	static Console c;
 	public static void main(String[] args) {
-		c = new Console(30, 100, 15, "Connect 4");
+		c = new Console(30, 100, 15, "Connect4");
+		int turn = 1;
+		boolean win = false;
 		boolean running = true;
 		gridDisplay();
 		while (running == true) {
-			c.print("Where would you like to place your token?");
-			tokenCheck(sc.nextInt());
+			c.print("Where would you like to place your token? ");
+			token(turn, columnCheck(c.readInt()), 440 - rowCheck(1));
+			if (turn == 1) {
+				turn = 2;
+			} else {
+				turn = 1;
+			}
+			if (win)
+				running = false;
 		}
-
 	}
 
 	public static void columnDisplay(int xValue, int yValue) {
@@ -28,34 +34,58 @@ public class CulminatingAssignment {
 			c.fillOval(xValue, yValue, 50, 50);
 	}
 
+
 	public static void gridDisplay() {
 		int[] x = {20, 90, 160, 230, 300, 370, 440};
-		int[] y = {20, 90, 160, 230, 300, 370};
+		int[] y = {70, 140, 210, 280, 350, 420};
 		c.setColour(Color.blue);
-		c.fillRect(5, 5, 500, 430);
+		c.fillRect(5, 55, 500, 430);
 		for (int i = 0; i < x.length; i++) {
 			for (int f = 0; f < y.length; f++)
 				columnDisplay(x[i], y[f]);
 		}
 	}
-	
-	public static boolean tokenCheck(int column) {
-		boolean[] column1 = new boolean[6];
-		for (int i = 0; i < column1.length; i++) {
-			if (column1[i] == true) {
-				
+
+	public static int rowCheck(int row) {
+		boolean[] row1 = new boolean[6];
+		for (int i = 0; i < row1.length; i++) {
+			if (!row1[i]) {
+				row1[i] = true;
+				row = (i+1)*20;
+				break;
+			} else if (row1[i] && !row1[i+1]) {
+				row1[i+1] = true;
+				row = (i+1)*20;
+				break;
 			}
 		}
-		return column1[0];
+		return row;
 	}
 	
-	public static void playerToken() {
-		int player = 1;
-		if (player == 1) {
-			c.fillOval
-		}
-		if (player == 2){
-			
-		}
+	public static int columnCheck(int column) {
+//		boolean[] row1 = new boolean[6];
+//		for (int i = 0; i < row1.length; i++) {
+//			if (!row1[i]) {
+//				row1[i] = true;
+//				column = (i+1)*20;
+//				break;
+//			} else if (row1[i] && !row1[i+1]) {
+//				row1[i+1] = true;
+//				column = (i+1)*20;
+//				break;
+//			}
+//		}
+		if (column < 8) {
+			return (column - 1)*70 + 20;
+		} else
+			return 0;
+	}
+
+	public static void token(int player, int column, int row) {
+		if (player == 1)
+			c.setColor(Color.red);
+		if (player == 2)
+			c.setColor(Color.yellow);
+		c.fillOval(column, row, 50, 50);
 	}
 }
