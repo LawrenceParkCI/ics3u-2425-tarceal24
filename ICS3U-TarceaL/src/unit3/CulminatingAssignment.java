af2531b0cf7db5a5b3fa6226d71a5f6d92b7a691
343500369 <343500369@MXL1502T88.tdsb.on.ca>
2025-01-16 11:40:48 -0500

    Updated CulminatingAssignment.java class

--- ICS3U-TarceaL/src/unit3/CulminatingAssignment.java
+++ ICS3U-TarceaL/src/unit3/CulminatingAssignment.java
@@ -23,64 +23,60 @@ public class CulminatingAssignment {
 	public static void main(String[] args) throws InterruptedException {
 		int playerTurn;
 		boolean game, running = true;
-		d = new Console(30, 100, 15, "Text");
+		d = new Console(10, 80, 15, "Text");
 		startGame();
-		c = new Console(30, 100, 15, "Connect4");
-		while (running = true) {
+		c = new Console(21, 57, 15, "Connect4");
+		while (running == true) {
 			displayGrid();
 			playerTurn = 1;
-			game = true;
-			while (game == true) {
+			while (game = true) {
 				d.setCursor(1,1);
 				d.setColor(Color.white);
 				d.fillRect(621, 0, 9, 21);
+				d.setCursor(5, 6);
 				d.print("Player " + playerTurn + "'s turn. Which column would you like to place your token in? ");
 				if (placeToken(playerTurn)) {
 					if (checkWin()) {
-						game = false;
-						typeOutput("Player " + playerTurn + " wins! Would you like to play again? ");
-						if (!d.readLine().equalsIgnoreCase("yes")) {
-							typeOutput("Thank you for playing :)");
-							running = false;
-							Thread.sleep(1400);
-							c.close();
-							d.close();
-						}
-						typeOutput("Alright. Restarting game.");
-						Thread.sleep(1000);
-						c.clear();
 						d.clear();
+						game = false;
+						d.setCursor(5, 18);
+						typeOutput("Player " + playerTurn + " wins! Would you like to play again? ");
+						String userInput = "";
+						while (!(userInput == "no" || userInput == "yes")) {
+							userInput = d.readLine();
+							d.clear();
+							d.setCursor(1, 1);
+							if (userInput.equalsIgnoreCase("no")) {
+								typeOutput("Thank you for playing :)");
+								running = false;
+								Thread.sleep(1470);
+								c.close();
+								d.close();
+							} else if (userInput.equalsIgnoreCase("no")) {
+								typeOutput("Alright. Restarting game.");
+								Thread.sleep(900);
+								c.clear();
+								d.clear();
+							} else {
+								d.setCursor(5, 18);
+								d.println("Player " + playerTurn + " wins! Would you like to play again? ");
+								typeOutput("Not a valid option. Try again ");
+							}
+						}
 					}
 					playerTurn = changePlayer(playerTurn);
 				} else {
-					// d.println("Not a valid option. Try again.");			
+					d.println("Not a valid option. Try again. ");
 				}
 			}
 		}
 	}
 
 	public static void startGame() throws InterruptedException {
-		String userInput = "";
-		while (!(userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("no"))) {
-			typeOutput("Hello... Would you like to play Connect 4? ");
-			userInput = d.readLine();
-			if (userInput.equalsIgnoreCase("yes")) {
-				typeOutput("Great!");
-				Thread.sleep(1000);
-			} else if (userInput.equalsIgnoreCase("no")) {
-				typeOutput("Too bad... See you next time.");
-				Thread.sleep(1000);
-				c.close();
-				d.close();
-			} else {
-				typeOutput("Not a valid response. Try again.");
-				Thread.sleep(1000);
-				d.clear();
-			}
-		}
-		d.setColor(Color.white);
-		// d.fillRect(0, 20, 400, 22);
-		// d.fillRect(0, 0, 420, 22);
+		d.setCursor(5, 18);
+		typeOutput("Welcome to Connect 4! Press Any Key To Start.");
+		d.getChar();
+		d.clear();
 	}
 
 	public static int changePlayer(int player) {
@@ -102,10 +98,10 @@ public class CulminatingAssignment {
 			for (int j = 0; j < rows; j++)
 				grid[j][i] = 20 + 70*i;
 
-		int[] column = {20, 90, 160, 230, 300, 370, 440};
-		int[] row = {70, 140, 210, 280, 350, 420};
+		int[] column = {15, 85, 155, 225, 295, 365, 435};
+		int[] row = {10, 80, 150, 220, 290, 360};
 		c.setColour(Color.blue);
-		c.fillRect(5, 55, 500, 430);
+		c.fillRect(0, 0, 500, 430);
 		for (int i = 0; i < columns; i++) {
 			for (int j = 0; j < rows; j++)
 				displayColumn(column[i], row[j]);
@@ -145,7 +141,7 @@ public class CulminatingAssignment {
 				d.print("Error - too many pieces in column");
 				return false;
 			} else {
-				c.fillOval(20 + column*70, 420 - row*70, 50, 50);
+				c.fillOval(15 + column*70, 360 - row*70, 50, 50);
 				cellValue[row][column] = player;
 			}
 		}
@@ -155,13 +151,13 @@ public class CulminatingAssignment {
 	}
 
 	public static boolean checkWin() {
-		for (int i = 0; i < columns - 4; i++) {
-			for (int j = 0; j < rows - 1; j++) {
+		for (int i = 0; i < columns - 3; i++) {
+			for (int j = 0; j < rows - 3; j++) {
 				if (cellValue[j][i] != 0 && cellValue[j][i] == cellValue[j][i+1] && 
 						cellValue[j][i+1] == cellValue[j][i+2] && cellValue[j][i+2] == cellValue[j][i+3]) { // 4 in a row
 					return true;
 				} else if (cellValue[j][i] != 0 && cellValue[j][i] == cellValue[j+1][i] &&
-						cellValue[j+1][i] == cellValue[j+2][i] && cellValue[j+2][i] == cellValue[j+3][i]){ // 4 in a column error
+						cellValue[j+1][i] == cellValue[j+2][i] && cellValue[j+2][i] == cellValue[j+3][i]){ // 4 in a column
 					return true;
 				} else if (cellValue[j][i] != 0 && cellValue[j][i] == cellValue[j+1][i+1] &&
 						cellValue[j+1][i+1] == cellValue[j+2][i+2] && cellValue[j+2][i+2] == cellValue[j+3][i+3]) { // 4 diagonal up-right/down-left
